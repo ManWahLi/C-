a9fdd18c9274bdef3c563a6a1ec47cf72bd21ac8
ManWahLi <105257849+ManWahLi@users.noreply.github.com>
2022-10-16 12:04:13 -0500

    Update password_validation.cs

--- password_validation.cs
+++ password_validation.cs
@@ -9,7 +9,10 @@ namespace Alogrithms
     {
       Console.WriteLine(isPasswordComplexity("Hell0"));
     }
-  
+    
+    // An alogrithms to check password complexity. A password must contain at least one uppercase, one lowercase and one number.
+    // param: input string
+    // return: bool
     static bool isPasswordComplexity(string s)
     {
       return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
